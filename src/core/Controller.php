f1deb52d1568912f1083b34d23bdb0a79b50d01e
MazBaz <mrlog42@gmail.com>
2023-12-03 01:20:06 +0100

    added response instance

--- src/core/Controller.php
+++ src/core/Controller.php
@@ -4,6 +4,12 @@ namespace app\core;
 
 class Controller
 {
+    public Response $response;
+    public function __construct()
+    {
+        $this->response = Application::$app->response;
+    }
+
     public function render($view, $params = []) {
         return Application::$app->router->renderView(str_replace(".", "/", $view), $params);
     }
