430753df7268e53ebc60220f3c9d8764fa0d3787
MazBaz <mrlog42@gmail.com>
2023-12-03 01:19:03 +0100

    fix bail, added custom errors messages

--- src/core/Validator.php
+++ src/core/Validator.php
@@ -4,41 +4,25 @@ namespace app\core;
 
 abstract class Validator
 {
-    public function validate($rules): array
+    private array $messages;
+
+    public function validate($rules, $messages = []): array
     {
         $errors = [];
+        $this->messages = $messages;
 
         foreach ($rules as $input => $rule) {
             $value = $this->input($input);
 
-            if ($value === null) return $errors;
+            if ($value === null) {
+                return $errors;
+            }
 
             // Split the rule into individual validation methods
             $validations = explode(';', $rule);
 
-            foreach ($validations as $validation) {
-                // Extract validation type and parameters (if any)
-                list($type, $params) = array_pad(explode(':', $validation, 2), 2, null);
-
-                if ($type === 'bail') {
-                    // If 'bail' is present, return errors immediately
-                    if (!empty($errors[$input])) {
-                        return $errors;
-                    }
-                } else {
-                    // Call the corresponding validation method
-                    $isValid = $this->{$type}($value, $params);
-
-                    if (!$isValid) {
-                        $errors[$input][] = $this->getErrorMessage($type, $input, $params);
-
-                        // If 'bail' is present, return errors immediately
-                        if ($this->hasBail($validations)) {
-                            return $errors;
-                        }
-                    }
-                }
-            }
+            // Check for 'bail' validation and return errors immediately
+            $errors = $this->bail($validations, $input, $value, $errors);
         }
 
         if (sizeof($errors) !== 0) {
@@ -49,11 +33,44 @@ abstract class Validator
 
             return $errors;
         }
+
         return $errors;
     }
 
+    private function bail($validations, $input, $value, $errors): array
+    {
+        if (!empty($errors[$input])) {
+            return $errors;
+        }
+
+        foreach ($validations as $validation) {
+            list($type, $params) = array_pad(explode(':', $validation, 2), 2, null);
+
+            if ($type !== 'bail') {
+                $isValid = $this->{$type}($value, $params);
+
+                if (!$isValid) {
+                    $errors[$input][] = $this->getErrorMessage($type, $input, $params);
+
+                    if ($this->hasBail($validations)) {
+                        return $errors;
+                    }
+                }
+            }
+        }
+
+        return $errors;
+    }
     private function getErrorMessage($type, $input, $params = null): string
     {
+        if (!empty($this->messages) && key_exists($input, $this->messages)) {
+            $messages = $this->messages[$input];
+
+            if (key_exists($type, $messages)) {
+                return $messages[$type];
+            }
+        }
+
         switch ($type) {
             case 'required':
                 return "The field '{$input}' is required.";
@@ -63,6 +80,8 @@ abstract class Validator
                 return "The field '{$input}' must be at most {$params} characters long.";
             case 'unique':
                 return "The {$input} already exist.";
+            case 'exist':
+                return "The {$input} does not exist in our data.";
             default:
                 return "Validation error for the field '{$input}'.";
         }
@@ -74,7 +93,7 @@ abstract class Validator
 
     private function required($value): bool
     {
-        return !empty($value);
+        return !empty($value) || $value !== "";
     }
 
     private function min($value, $minLength): bool
@@ -139,5 +158,16 @@ abstract class Validator
         $exist = $class->where([$col, $value]) !== false;
 
         return !$exist;
+    }
+
+    private function exist($value, $inputName) : bool {
+        $class = explode(",", $inputName)[0];
+        $col = explode(",", $inputName)[1];
+
+        $class = new $class();
+
+        $exist = $class->where([$col, $value]) !== false;
+
+        return $exist;
     }
 }
